5684331d5e8eed393fc8e5cf07e209166dedabd0
guli2103 <gulbahorcoder@gmail.com>
2023-02-23 23:41:46 +0500

    register

--- blog/backend/views.py
+++ blog/backend/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Forma ajoyib yakunlandi,endi loginga kiring ')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form' : form
+    }        
+    return render(request, 'register.html', context)
+
+
